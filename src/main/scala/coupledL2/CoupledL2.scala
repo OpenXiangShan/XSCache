OpenXiangShan/CoupledL2

fix(Arb): fix FastArbiter timing

src/main/scala/coupledL2/CoupledL2.scala
@@ -35,6 +35,7 @@ import huancun.{BankBitsKey, TPmetaReq, TPmetaResp}
 import utility.mbist.{MbistInterface, MbistPipeline}
 import utility.sram.{SramBroadcastBundle, SramHelper}
 import coupledL2.utils.ArbPerf
+import coupledL2.utils.L2FastArbiter
 
 trait HasCoupledL2Parameters {
   val p: Parameters
@@ -217,7 +218,7 @@ trait HasCoupledL2Parameters {
   }
 
   def fastArb[T <: Bundle](in: Seq[DecoupledIO[T]], out: DecoupledIO[T], name: Option[String] = None)(implicit p: Parameters): Unit = {
-    val arb = Module(new FastArbiter[T](chiselTypeOf(out.bits), in.size))
+    val arb = Module(new L2FastArbiter[T](chiselTypeOf(out.bits), in.size))
     if (name.nonEmpty) { arb.suggestName(s"${name.get}_arb") }
     for ((a, req) <- arb.io.in.zip(in)) { a <> req }
     out <> arb.io.out

src/main/scala/coupledL2/RequestBuffer.scala
@@ -98,7 +98,7 @@ class RequestBuffer(flow: Boolean = true, entries: Int = 4)(implicit p: Paramete
   io.ASet := io.in.bits.set
 
   val buffer = RegInit(VecInit(Seq.fill(entries)(0.U.asTypeOf(new ReqEntry))))
-  val issueArb = Module(new FastArbiter(new ReqEntry, entries))
+  val issueArb = Module(new L2FastArbiter(new ReqEntry, entries))
   ArbPerf(issueArb, "issueArb")
   val chosenQ = Module(new Queue(new ChosenQBundle(log2Ceil(entries)), entries = 1, pipe = true, flow = false))
   val chosenQValid = chosenQ.io.deq.valid

src/main/scala/coupledL2/SinkC.scala
@@ -22,8 +22,9 @@ import chisel3.util._
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.tilelink.TLMessages._
 import org.chipsalliance.cde.config.Parameters
-import utility.{MemReqSource, XSPerfAccumulate, RRArbiterInit}
+import utility.{MemReqSource, XSPerfAccumulate}
 import coupledL2.utils.ArbPerf
+import coupledL2.utils.L2FastArbiter
 
 class PipeBufferResp(implicit p: Parameters) extends L2Bundle {
   val data = Vec(beatSize, UInt((beatBytes * 8).W))
@@ -55,7 +56,7 @@ class SinkC(implicit p: Parameters) extends L2Module {
   val dataValids = VecInit(beatValids.map(_.asUInt.orR)).asUInt
   val taskBuf = RegInit(VecInit(Seq.fill(bufBlocks)(0.U.asTypeOf(new TaskBundle))))
   val taskValids = RegInit(VecInit(Seq.fill(bufBlocks)(false.B)))
-  val taskArb = Module(new RRArbiterInit(new TaskBundle, bufBlocks))
+  val taskArb = Module(new L2FastArbiter(new TaskBundle, bufBlocks))
   val bufValids = taskValids.asUInt | dataValids
   ArbPerf(taskArb, "taskArb")
 

src/main/scala/coupledL2/SourceB.scala
@@ -98,7 +98,7 @@ class SourceB(implicit p: Parameters) extends L2Module {
   }
 
   /* ======== Issue ======== */
-  val issueArb = Module(new FastArbiter(new SourceBReq, entries))
+  val issueArb = Module(new L2FastArbiter(new SourceBReq, entries))
   ArbPerf(issueArb, "issueArb")
   issueArb.io.in zip probes foreach{
     case (i, p) =>

src/main/scala/coupledL2/prefetch/BestOffsetPrefetch.scala
@@ -26,7 +26,7 @@
 
 package coupledL2.prefetch
 
-import utility.{ChiselDB, Constantin, MemReqSource, ParallelPriorityMux, RRArbiterInit, XSPerfAccumulate}
+import utility.{ChiselDB, Constantin, MemReqSource, ParallelPriorityMux, XSPerfAccumulate}
 import utility.sram.SRAMTemplate
 import org.chipsalliance.cde.config.Parameters
 import chisel3.DontCare.:=
@@ -37,6 +37,7 @@ import coupledL2.utils.ReplacementPolicy
 import scopt.Read
 import freechips.rocketchip.util.SeqToAugmentedSeq
 import coupledL2.utils.ArbPerf
+import coupledL2.utils.L2FastArbiter
 
 case class BOPParameters(
   virtualTrain: Boolean = true,
@@ -419,8 +420,8 @@ class PrefetchReqBuffer(name: String = "vbop")(implicit p: Parameters) extends B
   val valids = Seq.fill(REQ_FILTER_SIZE)(RegInit(false.B))
   val entries = Seq.fill(REQ_FILTER_SIZE)(Reg(new BopReqBufferEntry))
   //val replacement = ReplacementPolicy.fromString("plru", REQ_FILTER_SIZE)
-  val tlb_req_arb = Module(new RRArbiterInit(new L2TlbReq, REQ_FILTER_SIZE))
-  val pf_req_arb = Module(new RRArbiterInit(new PrefetchReq, REQ_FILTER_SIZE))
+  val tlb_req_arb = Module(new L2FastArbiter(new L2TlbReq, REQ_FILTER_SIZE))
+  val pf_req_arb = Module(new L2FastArbiter(new PrefetchReq, REQ_FILTER_SIZE))
   ArbPerf(tlb_req_arb, "bop_tlb_req_arb")
   ArbPerf(pf_req_arb, "bop_pf_req_arb")
 

src/main/scala/coupledL2/tl2chi/MMIOBridge.scala
@@ -28,6 +28,7 @@ import freechips.rocketchip.tilelink.TLMessages._
 import coupledL2.HasCoupledL2Parameters
 import coupledL2.{MemBackTypeMM, MemPageTypeNC}
 import coupledL2.utils.ArbPerf
+import coupledL2.utils.L2FastArbiter
 
 class MMIOBridge()(implicit p: Parameters) extends LazyModule
   with HasCoupledL2Parameters
@@ -382,7 +383,7 @@ class MMIOBridgeImp(outer: MMIOBridge) extends LazyModuleImp(outer)
     entry.io.id := i.U
   }
 
-  val txreqArb = Module(new RRArbiterInit(chiselTypeOf(io.tx.req.bits), mmioBridgeSize))
+  val txreqArb = Module(new L2FastArbiter(chiselTypeOf(io.tx.req.bits), mmioBridgeSize))
   ArbPerf(txreqArb, "mmio_txreq_arb")
   for ((a, entry) <- txreqArb.io.in.zip(entries)) {
     val req = entry.io.chi.tx.req

src/main/scala/coupledL2/tl2chi/TL2CHICoupledL2.scala
@@ -19,7 +19,7 @@ package coupledL2.tl2chi
 
 import chisel3._
 import chisel3.util._
-import utility.{FastArbiter, Pipeline, ParallelPriorityMux, RegNextN, RRArbiterInit, XSPerfAccumulate}
+import utility.{Pipeline, ParallelPriorityMux, RegNextN, XSPerfAccumulate}
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.tilelink.TLMessages._
@@ -29,6 +29,7 @@ import scala.math.max
 import coupledL2._
 import coupledL2.prefetch._
 import coupledL2.utils.ArbPerf
+import coupledL2.utils.L2FastArbiter
 
 abstract class TL2CHIL2Bundle(implicit val p: Parameters) extends Bundle
   with HasCoupledL2Parameters
@@ -129,7 +130,7 @@ class TL2CHICoupledL2(implicit p: Parameters) extends CoupledL2Base {
     slices match {
       case slices: Seq[Slice] =>
         // TXREQ
-        val txreq_arb = Module(new RRArbiterInit(new CHIREQ, slices.size + 1)) // plus 1 for MMIO
+        val txreq_arb = Module(new L2FastArbiter(new CHIREQ, slices.size + 1)) // plus 1 for MMIO
         ArbPerf(txreq_arb, "txreq_arb")
         val txreq = Wire(DecoupledIO(new CHIREQ))
         slices.zip(txreq_arb.io.in.init).foreach { case (s, in) => in <> s.io.out.tx.req }

OpenXiangShan/XSCache

src/main/scala/coupledL2/CoupledL2.scala
@@ -34,7 +34,7 @@ import coupledL2.prefetch._
 import huancun.{BankBitsKey, TPmetaReq, TPmetaResp}
 import utility.mbist.{MbistInterface, MbistPipeline}
 import utility.sram.{SramBroadcastBundle, SramHelper}
-import coupledL2.utils.ArbPerf
+import coupledL2.utils._
 
 trait HasCoupledL2Parameters {
   val p: Parameters
@@ -220,7 +220,7 @@ trait HasCoupledL2Parameters {
   }
 
   def fastArb[T <: Bundle](in: Seq[DecoupledIO[T]], out: DecoupledIO[T], name: Option[String] = None)(implicit p: Parameters): Unit = {
-    val arb = Module(new FastArbiter[T](chiselTypeOf(out.bits), in.size))
+    val arb = Module(new TwoLevelRRArbiter[T](chiselTypeOf(out.bits), in.size))
     if (name.nonEmpty) { arb.suggestName(s"${name.get}_arb") }
     for ((a, req) <- arb.io.in.zip(in)) { a <> req }
     out <> arb.io.out

src/main/scala/coupledL2/RequestBuffer.scala
@@ -98,7 +98,7 @@ class RequestBuffer(flow: Boolean = true, entries: Int = 4)(implicit p: Paramete
   io.ASet := io.in.bits.set
 
   val buffer = RegInit(VecInit(Seq.fill(entries)(0.U.asTypeOf(new ReqEntry))))
-  val issueArb = Module(new FastArbiter(new ReqEntry, entries))
+  val issueArb = Module(new TwoLevelRRArbiter(new ReqEntry, entries))
   ArbPerf(issueArb, "issueArb")
   val chosenQ = Module(new Queue(new ChosenQBundle(log2Ceil(entries)), entries = 1, pipe = true, flow = false))
   val chosenQValid = chosenQ.io.deq.valid

src/main/scala/coupledL2/SinkC.scala
@@ -22,8 +22,8 @@ import chisel3.util._
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.tilelink.TLMessages._
 import org.chipsalliance.cde.config.Parameters
-import utility.{MemReqSource, XSPerfAccumulate, RRArbiterInit}
-import coupledL2.utils.ArbPerf
+import utility.{MemReqSource, XSPerfAccumulate}
+import coupledL2.utils._
 
 class PipeBufferResp(implicit p: Parameters) extends L2Bundle {
   val data = Vec(beatSize, UInt((beatBytes * 8).W))
@@ -55,7 +55,7 @@ class SinkC(implicit p: Parameters) extends L2Module {
   val dataValids = VecInit(beatValids.map(_.asUInt.orR)).asUInt
   val taskBuf = RegInit(VecInit(Seq.fill(bufBlocks)(0.U.asTypeOf(new TaskBundle))))
   val taskValids = RegInit(VecInit(Seq.fill(bufBlocks)(false.B)))
-  val taskArb = Module(new RRArbiterInit(new TaskBundle, bufBlocks))
+  val taskArb = Module(new TwoLevelRRArbiter(new TaskBundle, bufBlocks))
   val bufValids = taskValids.asUInt | dataValids
   ArbPerf(taskArb, "taskArb")
 

src/main/scala/coupledL2/SourceB.scala
@@ -98,7 +98,7 @@ class SourceB(implicit p: Parameters) extends L2Module {
   }
 
   /* ======== Issue ======== */
-  val issueArb = Module(new FastArbiter(new SourceBReq, entries))
+  val issueArb = Module(new TwoLevelRRArbiter(new SourceBReq, entries))
   ArbPerf(issueArb, "issueArb")
   issueArb.io.in zip probes foreach{
     case (i, p) =>

src/main/scala/coupledL2/prefetch/BestOffsetPrefetch.scala
@@ -26,7 +26,7 @@
 
 package coupledL2.prefetch
 
-import utility.{ChiselDB, Constantin, MemReqSource, ParallelPriorityMux, RRArbiterInit, XSPerfAccumulate}
+import utility.{ChiselDB, Constantin, MemReqSource, ParallelPriorityMux, XSPerfAccumulate}
 import utility.sram.SRAMTemplate
 import org.chipsalliance.cde.config.Parameters
 import chisel3.DontCare.:=
@@ -36,7 +36,7 @@ import coupledL2.{HasCoupledL2Parameters, L2TlbReq, L2ToL1TlbIO, TlbCmd, Pbmt}
 import coupledL2.utils.ReplacementPolicy
 import scopt.Read
 import freechips.rocketchip.util.SeqToAugmentedSeq
-import coupledL2.utils.ArbPerf
+import coupledL2.utils._
 
 case class BOPParameters(
   virtualTrain: Boolean = true,
@@ -419,8 +419,8 @@ class PrefetchReqBuffer(name: String = "vbop")(implicit p: Parameters) extends B
   val valids = Seq.fill(REQ_FILTER_SIZE)(RegInit(false.B))
   val entries = Seq.fill(REQ_FILTER_SIZE)(Reg(new BopReqBufferEntry))
   //val replacement = ReplacementPolicy.fromString("plru", REQ_FILTER_SIZE)
-  val tlb_req_arb = Module(new RRArbiterInit(new L2TlbReq, REQ_FILTER_SIZE))
-  val pf_req_arb = Module(new RRArbiterInit(new PrefetchReq, REQ_FILTER_SIZE))
+  val tlb_req_arb = Module(new TwoLevelRRArbiter(new L2TlbReq, REQ_FILTER_SIZE))
+  val pf_req_arb = Module(new TwoLevelRRArbiter(new PrefetchReq, REQ_FILTER_SIZE))
   ArbPerf(tlb_req_arb, "bop_tlb_req_arb")
   ArbPerf(pf_req_arb, "bop_pf_req_arb")
 

src/main/scala/coupledL2/tl2chi/MMIOBridge.scala
@@ -27,7 +27,7 @@ import freechips.rocketchip.tilelink._
 import freechips.rocketchip.tilelink.TLMessages._
 import coupledL2.HasCoupledL2Parameters
 import coupledL2.{MemBackTypeMM, MemPageTypeNC}
-import coupledL2.utils.ArbPerf
+import coupledL2.utils._
 
 class MMIOBridge()(implicit p: Parameters) extends LazyModule
   with HasCoupledL2Parameters
@@ -382,7 +382,7 @@ class MMIOBridgeImp(outer: MMIOBridge) extends LazyModuleImp(outer)
     entry.io.id := i.U
   }
 
-  val txreqArb = Module(new RRArbiterInit(chiselTypeOf(io.tx.req.bits), mmioBridgeSize))
+  val txreqArb = Module(new TwoLevelRRArbiter(chiselTypeOf(io.tx.req.bits), mmioBridgeSize))
   ArbPerf(txreqArb, "mmio_txreq_arb")
   for ((a, entry) <- txreqArb.io.in.zip(entries)) {
     val req = entry.io.chi.tx.req

src/main/scala/coupledL2/tl2chi/TL2CHICoupledL2.scala
@@ -19,7 +19,7 @@ package coupledL2.tl2chi
 
 import chisel3._
 import chisel3.util._
-import utility.{FastArbiter, Pipeline, ParallelPriorityMux, RegNextN, RRArbiterInit, XSPerfAccumulate}
+import utility.{Pipeline, ParallelPriorityMux, RegNextN, XSPerfAccumulate}
 import freechips.rocketchip.diplomacy._
 import freechips.rocketchip.tilelink._
 import freechips.rocketchip.tilelink.TLMessages._
@@ -28,7 +28,7 @@ import org.chipsalliance.cde.config.{Parameters, Field}
 import scala.math.max
 import coupledL2._
 import coupledL2.prefetch._
-import coupledL2.utils.ArbPerf
+import coupledL2.utils._
 
 abstract class TL2CHIL2Bundle(implicit val p: Parameters) extends Bundle
   with HasCoupledL2Parameters
@@ -129,7 +129,7 @@ class TL2CHICoupledL2(implicit p: Parameters) extends CoupledL2Base {
     slices match {
       case slices: Seq[Slice] =>
         // TXREQ
-        val txreq_arb = Module(new RRArbiterInit(new CHIREQ, slices.size + 1)) // plus 1 for MMIO
+        val txreq_arb = Module(new TwoLevelRRArbiter(new CHIREQ, slices.size + 1)) // plus 1 for MMIO
         ArbPerf(txreq_arb, "txreq_arb")
         val txreq = Wire(DecoupledIO(new CHIREQ))
         slices.zip(txreq_arb.io.in.init).foreach { case (s, in) => in <> s.io.out.tx.req }