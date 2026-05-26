b95532b2c564d35f4e051e39125e9a3f5df428c4
lixin <1037997956@qq.com>
2026-05-26 17:04:52 +0800

    hint: fix hint assert bug

--- src/main/scala/coupledL2/CustomL1Hint.scala
+++ src/main/scala/coupledL2/CustomL1Hint.scala
@@ -108,14 +108,14 @@ class CustomL1Hint(implicit p: Parameters) extends L2Module {
   // noSpaceForSinkReq in GrantBuffer may ensure that these queues will not overflow
   assert(enq_s3.ready || !enq_s3.valid)
 
-  val hint_s1Queue = Module(new Pipeline(new HintQueueEntry))
-  hint_s1Queue.io.in.valid := valid_s1 && (!canFlow_s1 || !flow_s1.ready)
-  hint_s1Queue.io.in.bits  := enqBits_s1
-  assert(!valid_s1 || hint_s1Queue.io.in.ready || flow_s1.ready)
+  val hint_s1Queue = Module(new Queue(new HintQueueEntry, 4))
+  hint_s1Queue.io.enq.valid := valid_s1 && (!canFlow_s1 || !flow_s1.ready)
+  hint_s1Queue.io.enq.bits  := enqBits_s1
+  assert(!valid_s1 || hint_s1Queue.io.enq.ready || flow_s1.ready)
 
-  drop_s1.valid := hint_s1Queue.io.out.valid && !io.retry_s2
-  drop_s1.bits := hint_s1Queue.io.out.bits
-  hint_s1Queue.io.out.ready := drop_s1.ready || io.retry_s2
+  drop_s1.valid := hint_s1Queue.io.deq.valid && !io.retry_s2
+  drop_s1.bits := hint_s1Queue.io.deq.bits
+  hint_s1Queue.io.deq.ready := drop_s1.ready || io.retry_s2
 
   flow_s1.valid := valid_s1 && canFlow_s1
   flow_s1.bits := enqBits_s1
@@ -126,7 +126,7 @@ class CustomL1Hint(implicit p: Parameters) extends L2Module {
   val respWithDataFire = io.l1Hint.fire && io.l1Hint.bits.hasData
   hintQueue.io.deq.ready := io.l1Hint.ready && !RegNext(respWithDataFire, false.B)
 
-  io.l1Hint.valid := hintQueue.io.deq.valid && !(io.retry_s2 && !hint_s1Queue.io.out.valid) && !RegNext(respWithDataFire, false.B)
+  io.l1Hint.valid := hintQueue.io.deq.valid && !(io.retry_s2 && !hint_s1Queue.io.deq.valid) && !RegNext(respWithDataFire, false.B)
   io.l1Hint.bits.sourceId := hintQueue.io.deq.bits.source
   io.l1Hint.bits.isKeyword := hintQueue.io.deq.bits.isKeyword
   io.l1Hint.bits.hasData := hintQueue.io.deq.bits.hasData
