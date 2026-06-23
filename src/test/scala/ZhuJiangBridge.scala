837c8b16e93f7e6011a933bed3f8e6cdbea9039c
zhanglinjuan <zhanglinjuan16@mails.ucas.ac.cn>
2026-06-23 13:45:37 +0800

    submodule(ZhuJiang): paramterize `cfgAxiDataBits`

--- src/test/scala/ZhuJiangBridge.scala
+++ src/test/scala/ZhuJiangBridge.scala
@@ -11,65 +11,65 @@ import zhujiang.chi.{DataFlit, ReqFlit, RespFlit, SnoopFlit}
 import zhujiang.device.socket.{SocketDevSide, SocketIcnSideBundle}
 
 trait HasZhuJiangAXI4Bridge {
-  protected def connectZJMemToAXI4(zjMem: ExtAxiBundle, axi: AXI4Bundle): Unit = {
-    require(zjMem.params.addrBits <= axi.params.addrBits)
-    require(zjMem.params.dataBits == axi.params.dataBits)
-    require(zjMem.params.idBits <= axi.params.idBits)
-    require(zjMem.params.lenBits == axi.params.lenBits)
-    require(zjMem.params.sizeBits == axi.params.sizeBits)
-    require(zjMem.params.burstBits == axi.params.burstBits)
-    require(zjMem.params.lockBits == axi.params.lockBits)
-    require(zjMem.params.cacheBits == axi.params.cacheBits)
-    require(zjMem.params.qosBits == axi.params.qosBits)
+  protected def connectZJToAXI4(zjAxi: ExtAxiBundle, axi: AXI4Bundle): Unit = {
+    require(zjAxi.params.addrBits <= axi.params.addrBits)
+    require(zjAxi.params.dataBits == axi.params.dataBits)
+    require(zjAxi.params.idBits <= axi.params.idBits)
+    require(zjAxi.params.lenBits == axi.params.lenBits)
+    require(zjAxi.params.sizeBits == axi.params.sizeBits)
+    require(zjAxi.params.burstBits == axi.params.burstBits)
+    require(zjAxi.params.lockBits == axi.params.lockBits)
+    require(zjAxi.params.cacheBits == axi.params.cacheBits)
+    require(zjAxi.params.qosBits == axi.params.qosBits)
 
-    axi.aw.valid := zjMem.awvalid
-    zjMem.awready := axi.aw.ready
-    axi.aw.bits.id := zjMem.awid
-    axi.aw.bits.addr := zjMem.awaddr
-    axi.aw.bits.len := zjMem.awlen
-    axi.aw.bits.size := zjMem.awsize
-    axi.aw.bits.burst := zjMem.awburst
-    axi.aw.bits.lock := zjMem.awlock
-    axi.aw.bits.cache := zjMem.awcache
-    axi.aw.bits.prot := zjMem.awprot
-    axi.aw.bits.qos := zjMem.awqos
+    axi.aw.valid := zjAxi.awvalid
+    zjAxi.awready := axi.aw.ready
+    axi.aw.bits.id := zjAxi.awid
+    axi.aw.bits.addr := zjAxi.awaddr
+    axi.aw.bits.len := zjAxi.awlen
+    axi.aw.bits.size := zjAxi.awsize
+    axi.aw.bits.burst := zjAxi.awburst
+    axi.aw.bits.lock := zjAxi.awlock
+    axi.aw.bits.cache := zjAxi.awcache
+    axi.aw.bits.prot := zjAxi.awprot
+    axi.aw.bits.qos := zjAxi.awqos
     axi.aw.bits.user := DontCare
     axi.aw.bits.echo := DontCare
 
-    axi.ar.valid := zjMem.arvalid
-    zjMem.arready := axi.ar.ready
-    axi.ar.bits.id := zjMem.arid
-    axi.ar.bits.addr := zjMem.araddr
-    axi.ar.bits.len := zjMem.arlen
-    axi.ar.bits.size := zjMem.arsize
-    axi.ar.bits.burst := zjMem.arburst
-    axi.ar.bits.lock := zjMem.arlock
-    axi.ar.bits.cache := zjMem.arcache
-    axi.ar.bits.prot := zjMem.arprot
-    axi.ar.bits.qos := zjMem.arqos
+    axi.ar.valid := zjAxi.arvalid
+    zjAxi.arready := axi.ar.ready
+    axi.ar.bits.id := zjAxi.arid
+    axi.ar.bits.addr := zjAxi.araddr
+    axi.ar.bits.len := zjAxi.arlen
+    axi.ar.bits.size := zjAxi.arsize
+    axi.ar.bits.burst := zjAxi.arburst
+    axi.ar.bits.lock := zjAxi.arlock
+    axi.ar.bits.cache := zjAxi.arcache
+    axi.ar.bits.prot := zjAxi.arprot
+    axi.ar.bits.qos := zjAxi.arqos
     axi.ar.bits.user := DontCare
     axi.ar.bits.echo := DontCare
 
-    axi.w.valid := zjMem.wvalid
-    zjMem.wready := axi.w.ready
-    axi.w.bits.data := zjMem.wdata
-    axi.w.bits.strb := zjMem.wstrb
-    axi.w.bits.last := zjMem.wlast.asBool
+    axi.w.valid := zjAxi.wvalid
+    zjAxi.wready := axi.w.ready
+    axi.w.bits.data := zjAxi.wdata
+    axi.w.bits.strb := zjAxi.wstrb
+    axi.w.bits.last := zjAxi.wlast.asBool
     axi.w.bits.user := DontCare
 
-    zjMem.bvalid := axi.b.valid
-    axi.b.ready := zjMem.bready
-    zjMem.bid := axi.b.bits.id
-    zjMem.bresp := axi.b.bits.resp
-    zjMem.buser := 0.U
+    zjAxi.bvalid := axi.b.valid
+    axi.b.ready := zjAxi.bready
+    zjAxi.bid := axi.b.bits.id
+    zjAxi.bresp := axi.b.bits.resp
+    zjAxi.buser := 0.U
 
-    zjMem.rvalid := axi.r.valid
-    axi.r.ready := zjMem.rready
-    zjMem.rid := axi.r.bits.id
-    zjMem.rdata := axi.r.bits.data
-    zjMem.rresp := axi.r.bits.resp
-    zjMem.rlast := axi.r.bits.last
-    zjMem.ruser := 0.U
+    zjAxi.rvalid := axi.r.valid
+    axi.r.ready := zjAxi.rready
+    zjAxi.rid := axi.r.bits.id
+    zjAxi.rdata := axi.r.bits.data
+    zjAxi.rresp := axi.r.bits.resp
+    zjAxi.rlast := axi.r.bits.last
+    zjAxi.ruser := 0.U
   }
 }
 
@@ -77,15 +77,24 @@ trait HasCHIToZhuJiangBridge {
   protected def connectCHIToZhuJiang(
     chi: DecoupledPortIO,
     socketIO: SocketIcnSideBundle,
-    ccNode: Node
-  )(implicit p: Parameters): Unit = {
+    ccNode: Node,
+    zjParams: Parameters
+  ): Unit = {
+    implicit val p: Parameters = zjParams
     require(ccNode.nodeType == NodeType.CC)
     require(ccNode.socket == "sync")
+    require(
+      chi.tx.dat.bits.dataCheck.isEmpty && chi.rx.dat.bits.dataCheck.isEmpty,
+      "ZhuJiang bridge requires CHI DataCheck disabled"
+    )
+    require(
+      chi.tx.dat.bits.poison.isEmpty && chi.rx.dat.bits.poison.isEmpty,
+      "ZhuJiang bridge requires CHI Poison disabled"
+    )
 
     val socket = Module(new SocketDevSide(ccNode))
     socketIO <> socket.io.socket
 
-    // CHI -> ZhuJiang icn.rx
     val txreqBits = Wire(new ReqFlit)
     mapReq(txreqBits, chi.tx.req.bits)
     socket.io.icn.rx.req.get.valid := chi.tx.req.valid
@@ -117,7 +126,6 @@ trait HasCHIToZhuJiangBridge {
       dbg.bits := 0.U.asTypeOf(dbg.bits)
     }
 
-    // ZhuJiang icn.tx -> CHI
     val rxrspBits = Wire(new CHIRSP)
     mapRsp(rxrspBits, socket.io.icn.tx.resp.get.bits.asTypeOf(new RespFlit))
     chi.rx.rsp.valid := socket.io.icn.tx.resp.get.valid
@@ -176,74 +184,9 @@ trait HasCHIToZhuJiangBridge {
     dst.DBID := src.dbID
   }
 
-  protected def mapDat(dst: DataFlit, src: CHIDAT): Unit = {
-    dst := 0.U.asTypeOf(dst)
-    dst.QoS := src.qos
-    dst.TgtID := src.tgtID
-    dst.SrcID := src.srcID
-    dst.TxnID := src.txnID
-    dst.HomeNID := src.homeNID
-    dst.Opcode := src.opcode
-    dst.RespErr := src.respErr
-    dst.Resp := src.resp
-    dst.DataSource := src.dataSource
-    dst.CBusy := src.cBusy.getOrElse(0.U)
-    dst.DBID := src.dbID
-    dst.DataID := src.dataID
-    dst.RSVDC := src.rsvdc
-    dst.BE := src.be
-    dst.Data := src.data
-  }
-
-  protected def mapSnp(dst: SnoopFlit, src: CHISNP): Unit = {
-    dst := 0.U.asTypeOf(dst)
-    dst.QoS := src.qos
-    dst.TgtID := src.fwdNID
-    dst.SrcID := src.srcID
-    dst.TxnID := src.txnID
-    dst.FwdNID := src.fwdNID
-    dst.FwdTxnID := src.fwdTxnID
-    dst.Opcode := src.opcode
-    dst.Addr := src.addr
-    dst.DoNotGoToSD := src.doNotGoToSD
-    dst.RetToSrc := src.retToSrc
-    dst.MECID := src.mpam.map(_.partID).getOrElse(0.U)
-    dst.MPAM := src.mpam.map(_.partID).getOrElse(0.U)
-  }
-
-  protected def mapReq(dst: CHIREQ, src: ReqFlit): Unit = {
-    dst := 0.U.asTypeOf(dst)
-    dst.qos := src.QoS
-    dst.tgtID := src.TgtID
-    dst.srcID := src.SrcID
-    dst.txnID := src.TxnID
-    dst.opcode := src.Opcode
-    dst.size := src.Size
-    dst.addr := src.Addr
-    dst.order := src.Order
-    dst.memAttr := MemAttr(
-      allocate = src.MemAttr(3),
-      cacheable = src.MemAttr(2),
-      device = src.MemAttr(1),
-      ewa = src.MemAttr(0)
-    )
-    dst.snpAttr := src.SnpAttr
-    dst.snoopMe := src.Excl
-    dst.expCompAck := src.ExpCompAck
-    dst.rsvdc := src.RSVDC
-    dst.mpam.foreach { m =>
-      m.perfMonGroup := 0.U
-      m.partID := src.PBHA
-      m.mpamNS := false.B
-    }
-    dst.returnTxnID := src.ReturnTxnID.getOrElse(0.U)
-    dst.returnNID := src.ReturnNID.getOrElse(0.U)
-  }
-
   protected def mapRsp(dst: CHIRSP, src: RespFlit): Unit = {
     dst := 0.U.asTypeOf(dst)
     dst.qos := src.QoS
-    dst.tgtID := src.TgtID
     dst.srcID := src.SrcID
     dst.txnID := src.TxnID
     dst.opcode := src.Opcode
@@ -252,6 +195,25 @@ trait HasCHIToZhuJiangBridge {
     dst.fwdState := src.FwdState
     dst.cBusy.foreach(_ := src.CBusy)
     dst.dbID := src.DBID
+    dst.pCrdType := 0.U
+  }
+
+  protected def mapDat(dst: DataFlit, src: CHIDAT): Unit = {
+    dst := 0.U.asTypeOf(dst)
+    dst.QoS := src.qos
+    dst.TgtID := src.tgtID
+    dst.SrcID := src.srcID
+    dst.TxnID := src.txnID
+    dst.Opcode := src.opcode
+    dst.RespErr := src.respErr
+    dst.Resp := src.resp
+    dst.FwdState := src.fwdState
+    dst.DataSource := src.dataSource
+    dst.CBusy := src.cBusy.getOrElse(0.U)
+    dst.DBID := src.dbID
+    dst.DataID := src.dataID
+    dst.Data := src.data
+    dst.BE := src.be
   }
 
   protected def mapDat(dst: CHIDAT, src: DataFlit): Unit = {
@@ -267,10 +229,11 @@ trait HasCHIToZhuJiangBridge {
     dst.dataSource := src.DataSource
     dst.cBusy.foreach(_ := src.CBusy)
     dst.dbID := src.DBID
+    dst.ccID := 0.U
     dst.dataID := src.DataID
-    dst.rsvdc := src.RSVDC
-    dst.be := src.BE
     dst.data := src.Data
+    dst.be := src.BE
+    dst.poison.foreach(_ := 0.U)
   }
 
   protected def mapSnp(dst: CHISNP, src: SnoopFlit): Unit = {
@@ -282,12 +245,8 @@ trait HasCHIToZhuJiangBridge {
     dst.fwdTxnID := src.FwdTxnID
     dst.opcode := src.Opcode
     dst.addr := src.Addr
+    dst.ns := false.B
     dst.doNotGoToSD := src.DoNotGoToSD
     dst.retToSrc := src.RetToSrc
-    dst.mpam.foreach { m =>
-      m.perfMonGroup := 0.U
-      m.partID := src.MECID
-      m.mpamNS := false.B
-    }
   }
 }
