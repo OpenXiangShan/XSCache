4e1a23190e9f0768f36f8513ae2cf6e364da0749
Yanqin Li <liyanqin@bosc.ac.cn>
2026-06-29 00:59:21 +0800

    fix(pfctrl): fix compile error

--- src/main/scala/coupledL2/prefetch/Prefetcher.scala
+++ src/main/scala/coupledL2/prefetch/Prefetcher.scala
@@ -203,7 +203,6 @@ class PrefetchIO(implicit p: Parameters) extends PrefetchBundle {
 }
 
 class PrefetchTopIO(implicit p: Parameters) extends PrefetchBundle {
-  val banks = 1 << bankBits
   val train = Vec(banks, Flipped(DecoupledIO(new PrefetchTrain)))
   val tlb_req = new L2ToL1TlbIO(nRespDups= 1)
   val req = Vec(banks, DecoupledIO(new PrefetchReq))
@@ -231,9 +230,6 @@ class Prefetcher(implicit p: Parameters) extends PrefetchModule {
   val pfFeedbackVec = IO(Input(Vec(banks, new PrefetchFeedbackBundle())))
 
   val prefetchController = Module(new PrefetchController)
-  prefetchController.io.isDemandTrain := io.train.valid && (
-    MemReqSource.isCPUReq(io.train.bits.reqsource) || MemReqSource.isL1Prefetch(io.train.bits.reqsource)
-  )
   prefetchController.io.pfFeedbackVec := pfFeedbackVec
 
   // l2 receive need 2 cycles to transmit from core
@@ -346,6 +342,9 @@ class Prefetcher(implicit p: Parameters) extends PrefetchModule {
   fastArb(io.train, train, Some("prefetch_train"))
   fastArb(io.resp, resp, Some("prefetch_resp"))
 
+  prefetchController.io.isDemandTrain := train.valid && (
+    MemReqSource.isCPUReq(train.bits.reqsource) || MemReqSource.isL1Prefetch(train.bits.reqsource)
+  )
   // =================== Connection for each Prefetcher =====================
   // Rcv > NL > VBOP > PBOP > TP > CDP
   train.ready := true.B
