badd8b6307fc0835b6a84b7383d6789b8c91ed36
yangzechen <yangzechen21@mails.ucas.ac.cn>
2026-07-26 17:16:13 +0800

    chore(CDP): add head lines

--- src/main/scala/coupledL2/prefetch/CDP.scala
+++ src/main/scala/coupledL2/prefetch/CDP.scala
@@ -1,3 +1,27 @@
+/** *************************************************************************************
+ * Copyright (c) 2020-2021 Institute of Computing Technology, Chinese Academy of Sciences
+ * Copyright (c) 2020-2021 Peng Cheng Laboratory
+ *
+ * XiangShan is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ * http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ *
+ *
+ * Acknowledgement
+ *
+ * This implementation is inspired by several key papers:
+ * [1] Robert Cooksey, Stephan Jourdan, and Dirk Grunwald. "[A stateless, content-directed data prefetching 
+ * mechanism.](https://doi.org/10.1145/635508.605427)" ACM SIGOPS Operating System Review. 2002
+ * *************************************************************************************
+ */
+
 package xscache.coupledL2.prefetch
 
 import chisel3._
